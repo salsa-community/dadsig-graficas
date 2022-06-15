756d8c1467a24be7810950b1252e4fc4f9fa2edf
Paulina Pradel <pradel.paulina@gmail.com>
2022-06-15 09:25:29 -0500

    datasets dummies para donas

--- src/components/donas/DaiDonas.vue
+++ src/components/donas/DaiDonas.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>Probando</div>
+  <div :id="dona_id" class="contenedor-dona">
+
+  </div>
 </template>
 
 <script>
+
+import * as d3 from "d3";
+
 export default {
   name: "DaiDonas"
 }
