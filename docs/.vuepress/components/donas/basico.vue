86d272876600b01e2dac075d693d1257add6888d
Paulina Pradel <pradel.paulina@gmail.com>
2022-06-20 03:38:32 -0500

    quitar arreglo de datos por default

--- docs/.vuepress/components/donas/basico.vue
+++ docs/.vuepress/components/donas/basico.vue
@@ -20,33 +20,6 @@ export default {
       datos: donasejemplo,
     }
   },
-  // data: function () {
-  //   return {
-  //     datos:[
-  //       {nombre:'Nombre de variable 1', cantidad:100, color:'#000'},
-  //       {nombre:'Nombre de variable 2', cantidad:120, color:'#191919'},
-  //       {nombre:'Nombre de variable 3', cantidad:60, color:'#333'},
-  //       {nombre:'Nombre de variable 4', cantidad:50, color:'#4c4c4c'},
-  //       {nombre:'Nombre de variable 6', cantidad:40, color:'#7b7b7b'},
-  //       {nombre:'Nombre de variable 5', cantidad:40, color:'#ccc'}
-  //     ],
-  //   }
-  // },
-  // methods: {
-  //   cambiaBase: function () {
-  //     let nuevos_datos = [];
-  //     let tamanio = 1 + Math.floor(Math.random()*6);
-  //     for(var i = 0 ; i < tamanio; i++) {
-  //       nuevos_datos.push({
-  //         nombre: "Nombre de variable " + i,
-  //         cantidad: parseInt(10+Math.random()*2000),
-  //         color: `rgba(${parseInt(Math.random()*255)}, ${parseInt(Math.random()*255)}, ${parseInt(Math.random()*255)})`
-  //       })
-  //     }
-  //     this.datos = nuevos_datos;
-  //     console.log(this.datos)
-  //   }
-  // }
 }
 </script>
 
