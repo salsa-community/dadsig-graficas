476635b4ac1420f6b405ed0ddfcbe19eef3fe3cd
Paulina Pradel <pradel.paulina@gmail.com>
2022-06-20 03:39:39 -0500

    quitar estilos de componente básico

--- docs/.vuepress/components/donas/basico.vue
+++ docs/.vuepress/components/donas/basico.vue
@@ -24,24 +24,4 @@ export default {
 </script>
 
 <style lang="scss">
-//body {
-//  font-family: "Montserrat", Arial, Helvetica, sans-serif;
-//  margin: 20px;
-//}
-//.boton-base {
-//  background-color: var(--color-acento-fondo);
-//  border: none;
-//  border-radius: 3px;
-//  color: var(--color-acento-texto);
-//  cursor: pointer;
-//  font-family: "Montserrat", Arial, Helvetica, sans-serif;
-//  margin-bottom: 20px;
-//  padding: 10px 20px;
-//  transition: background-color .33s ease-in-out, color .3s ease-in-out;
-//  &:hover,
-//  &:focus {
-//    background-color: var(--color-acento-fondo-hover);
-//    color: var(--color-acento-texto-hover);
-//  }
-//}
 </style>
