d4d45347513575eb11d30c7a2e64a16d7ed6ba12
Paulina Pradel <pradel.paulina@gmail.com>
2022-06-27 01:50:27 -0500

    logo en gráfica de líneas

--- src/components/lineas/DadsigLineas.vue
+++ src/components/lineas/DadsigLineas.vue
@@ -43,6 +43,11 @@
 
     </div>
     <slot name="pie"></slot>
+    <div v-show="logo_conacyt" class="grid-column-4 grid-column-6-esc">
+      <a class="boton boton-conacyt" href="https://conacyt.mx/" target="_blank">
+        <img src="https://conacyt.mx/wp-content/uploads/2021/10/logo_conacyt_con_sintagma_azul_completo.svg" alt="Conacyt" height="28px">
+      </a>
+    </div>
 
   </div>
 </template>
@@ -93,6 +98,12 @@ export default {
     tipo_tooltip: {
       type: String,
       default: () => "general"
+    },
+    logo_conacyt: {
+      type: Boolean,
+      default: function () {
+        return true
+      }
     },
     tooltip_activo: {
       type: Boolean,
